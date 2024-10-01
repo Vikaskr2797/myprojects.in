38fa24d2890d26309a8a68c53818cd8421ce0883
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2024-10-01 17:55:09 +0530

    Update db_config.php
    changed connection towards db

--- constants/db_config.php
+++ constants/db_config.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "job_portal";
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "job_portal";
 ?>
 
 <?php
-//$servername = "localhost";
-//$username = "dreamers";
-//$password = "FV;o]tKxQV9i";
-//$dbname = "dreamers_job_portal";
+$servername = "localhost";
+$username = "dreamers";
+$password = "FV;o]tKxQV9i";
+$dbname = "dreamers_job_portal";
 ?>
